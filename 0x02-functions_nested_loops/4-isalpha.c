e01d78a621ac06d636855a48de95c4ed2bd55ed7
Poesitor <Poesitor1@gmail.com>
2023-03-22 21:48:44 +0000

    solved task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * Return: 1 (character is an alphabet), 0 (character is not an alphabet)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 61 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
