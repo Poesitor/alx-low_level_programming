2c4bacffd9f56a0589bd01675acf66f7c8514eca
Poesitor <Poesitor1@gmail.com>
2023-03-23 09:57:09 +0000

    solved task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @n: number to start from
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", 98);
+		return;
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
+		printf("%d\n", 98);
+		return;
+	}
+	else
+	{
+		printf("%d\n", 98);
+	}
+}
