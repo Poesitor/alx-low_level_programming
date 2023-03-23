caaa45244cc5484e06f205176c1737806cad270c
Poesitor <Poesitor1@gmail.com>
2023-03-23 17:19:24 +0000

    considered a case of same numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,16 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if ((a >= b && b >= c) || (a >= c && c >= b))
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if ((b >= a && a >= c) || (b >= c && b >= a))
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
+	else if ((c >= a && a >= b) || (c >= b && b >= a))
+
 	{
 		largest = c;
 	}
