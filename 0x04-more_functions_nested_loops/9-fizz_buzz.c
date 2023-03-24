8eafbc51c73374f1a2b4a19567bf5dab8e25b3c6
Poesitor <Poesitor1@gmail.com>
2023-03-24 08:12:17 +0000

    fizz buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers 1 tp 100. 
+ * for multiples of therr, prints Fizz instead
+ * for multiples of five prints Buzz instead. 
+ * for numbers which are multiples of both three and five print FizzBuzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
