6360c84ea555c5f98ec3cefacde3e43214c8dfe5
Poesitor <Poesitor1@gmail.com>
2023-04-07 07:07:01 +0000

    follow betty style

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,11 +19,11 @@ char *_strncat(char *dest, char *src, int n)
 		len++;
 		j++;
 	}
-	
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[len + i] = src[i];
-	}	
+	}
 	dest[len + i] = '\0';
 
 	return (dest);
