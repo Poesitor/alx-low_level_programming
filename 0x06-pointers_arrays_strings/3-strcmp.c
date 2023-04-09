035d496ed8551741298d346271def025f632fc05
Poesitor <Poesitor1@gmail.com>
2023-04-09 12:07:36 +0000

    a function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 (s1 and s2 are equal), 
+ * -1 (s1 is less than s2)
+ *  1 (s1 is greater than s2)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, val1, val2;
+
+	val1 = 0;
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		val1 += s1[i];
+	}
+
+	val2 = 0;
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		val2 += s2[i];
+	}
+
+	if (val1 < val2)
+		return (-1);
+	if (val1 > val2)
+		return (1);
+
+	return (0);
+}
