d08f304f06e66dd6ef4eb900096c8a7169224980
Poesitor <Poesitor1@gmail.com>
2023-04-09 13:22:18 +0000

    remove trailing white space

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: 0 (s1 and s2 are equal), 
+ * Return: 0 (s1 and s2 are equal),
  * -1 (s1 is less than s2)
  *  1 (s1 is greater than s2)
  */
