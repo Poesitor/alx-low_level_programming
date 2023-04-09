69bb9162f46b5115e451330d5d6da570471a377c
Poesitor <Poesitor1@gmail.com>
2023-04-09 13:18:16 +0000

    compare each character of the string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,21 +12,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (*s1 && (*s1 == *s2))
 	{
-		if (s1[i] < s2[i])
-			return (-15);
-		if (s1[i] > s2[i])
-			return (15);
+		s1++;
+		s2++;
 	}
 
-	if (s1[i] == '\0' && s2[i] == '\0')
-		return (0);
-	else if (s1[i] == '\0')
-		return (-15);
-
-	return (1);
-
+	return (*s1 - *s2);
 }
