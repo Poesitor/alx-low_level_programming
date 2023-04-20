e1980aa851aa27d4dac525cb03ed1d0f9c032bbd
Poesitor <Poesitor1@gmail.com>
2023-04-20 12:09:29 +0000

    used NULL from stdio.h

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strchr - a function that locates a character in a string
@@ -24,5 +25,5 @@ char *_strchr(char *s, char c)
 		return (s + i);
 	}
 
-	return ('\0');
+	return (NULL);
 }
