1abca1d9cbc7b2280645086aa3b415d7f5812064
Poesitor <Poesitor1@gmail.com>
2023-05-02 14:20:49 +0000

    changed code logic

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 
 /**
  * argstostr - concatenates all the arguments of a program
@@ -11,36 +11,29 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, len = 0, total_len = 0;
-	char *str;
+	int i, position, len = 0, total_len = 0;
+	char *result;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		len = 0;
-		while (av[i][len])
-			len++;
-		total_len += len + 1;
+		total_length += strlen(av[i]) + 1;
 	}
 
-	str = malloc(sizeof(char) * total_len);
-	if (str == NULL)
+	result = malloc(sizeof(char) * total_len);
+	if (result == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		len = 0;
-		while (av[i][len])
-		{
-			str[j] = av[i][len];
-			len++;
-			j++;
-		}
-		str[j] = '\n';
-		j++;
+		strcpy(&result[position], av[i]);
+		position += strlen(av[i]);
+		result[position] = '\n';
+		position++;
 	}
-	str[j] = '\0';
-	return (str);
+
+	result[position] = '\0';
+	return (result);
 }
