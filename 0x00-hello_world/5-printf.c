d6d4eda1379b8041c0ee8d3dd223fd3982ee7265
Poesitor <Poesitor1@gmail.com>
2023-06-21 23:51:02 +0000

    function that prints with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints with printf
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
