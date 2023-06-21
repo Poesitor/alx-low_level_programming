de97a619939ccaae17e300f15c29a520f4147186
Poesitor <Poesitor1@gmail.com>
2023-06-21 23:35:10 +0000

    c program that prints a given text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints a message
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
