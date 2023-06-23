d3ab045b982316f54ff6a9215d5a7d15abce967b
Poesitor <Poesitor1@gmail.com>
2023-06-23 15:11:28 +0000

    a function to add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @num1: the first integer
+ * @num2: the second integer
+ *
+ * Return: the sum of num1 and num2
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
