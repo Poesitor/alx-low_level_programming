5877242edd682498c9a5df2bca7a9d2c36cc75a6
Poesitor <Poesitor1@gmail.com>
2023-06-23 10:49:29 +0000

    print every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; (i != 2 ? j < 10 : j < 4) ; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
