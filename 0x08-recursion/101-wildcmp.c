57b1a7cf943f8b5d6be09c9bd743a66ebe08d16c
Poesitor <Poesitor1@gmail.com>
2023-07-01 16:11:47 +0000

    second try

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,29 +1,28 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and checks if they can be
- * considered identical
+ * wildcmp_helper - recursive helper function for wildcmp.
  *
  * @s1: The first string.
  * @s2: The second string.
  *
  * Return: 1 if the strings can be considered identical, 0 otherwise.
  */
-int wildcmp(char *s1, char *s2)
+int wildcmp_helper(char *s1, char *s2)
 {
 	if (*s2 == '*')
 	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp_helper(s1, s2 + 1));
 		if (*(s2 + 1) == '\0')
 			return (1);
-		if (*s1 == '\0')
+		while (*s1 != '\0')
 		{
-			if (*(s2 + 1) == '*')
-				return (wildcmp(s1, s2 + 1));
-			return (0);
+			if (wildcmp_helper(s1, s2 + 1))
+				return (1);
+			s1++;
 		}
-		if (*(s2 + 1) == '*')
-			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2 + 1));
+		return wildcmp_helper(s1, s2 + 1);
 	}
 	if (*s1 != *s2)
 		return (0);
