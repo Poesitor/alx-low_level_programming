cb86ac4265eefb27df4c6f5b79312154e22f9f46
Poesitor <Poesitor1@gmail.com>
2023-07-01 16:14:40 +0000

    third try at wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -30,3 +30,16 @@ int wildcmp_helper(char *s1, char *s2)
 		return (1);
 	return (wildcmp(s1 + 1, s2 + 1));
 }
+
+/**
+ * wildcmp - compares two strings and checks if they can be considered identical
+ *
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: 1 if the strings can be considered identical, 0 otherwise.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (wildcmp_helper(s1, s2));
+}
