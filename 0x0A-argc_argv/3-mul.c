2361adb5b6d28af31466b4433b417001df1d98d2
Poesitor <Poesitor1@gmail.com>
2023-07-02 17:54:53 +0000

    a function that multiplies the two arguments passed to it

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers
+ *
+ * @argc: argument count
+ * @argv: an array of arguments passed to main
+ *
+ * Return: Always 0. (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
