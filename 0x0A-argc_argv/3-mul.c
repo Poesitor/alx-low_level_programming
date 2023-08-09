c3a4438a0363239af2451bd9367b0e154d15fc19
Poesitor <Poesitor1@gmail.com>
2023-08-09 09:46:00 +0000

    a program to multiply to arguments passed to it

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies and prints two numbers passed to it
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if two arguments are passed
+ * otherwise returns 1.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		puts("Error");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	
+	return (0);
+}
