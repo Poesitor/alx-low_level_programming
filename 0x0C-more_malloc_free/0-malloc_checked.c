4c8035bfe5238a489e9ec84066ead192d7f7d158
Poesitor <Poesitor1@gmail.com>
2023-08-10 14:02:53 +0000

    a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ *
+ * @b: The size of the memory to be allocated
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *space;
+
+	space = malloc(b);
+	if (space == NULL)
+		exit(98);
+}
