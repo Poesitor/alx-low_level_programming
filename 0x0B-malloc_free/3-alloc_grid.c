481f0153a2efee72e85029e6b8fc9526e1356176
Poesitor <Poesitor1@gmail.com>
2023-08-10 10:53:19 +0000

    second try at task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,13 +17,13 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * height);
+	arr = (int **) malloc(sizeof(int *) * height);
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(sizeof(int) * width);
+		arr[i] = (int *) malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
@@ -33,7 +33,10 @@ int **alloc_grid(int width, int height)
 			free(arr);
 			return (NULL);
 		}
+	}
 
+	for (i = 0; i < height; i++)
+	{
 		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;
