903af557278292c22d3d729ded58846300380bd5
Poesitor <Poesitor1@gmail.com>
2023-08-15 09:39:13 +0000

    reformat ternary statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		(d->name != NULL) ? printf("Name: %s\n", d->name) : printf("nil");
-		(d->age != 0) ? printf("Age: %f\n", d->age) : printf("nil");
-		(d->owner != NULL) ? printf("Owner: %s\n", d->owner) : printf("nil");
+		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 	}
 }
