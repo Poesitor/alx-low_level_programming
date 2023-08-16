83b66f35e59f4f87289eaee87fac0e49da5c1f18
Poesitor <Poesitor1@gmail.com>
2023-08-16 00:22:04 +0000

    point the strings to a new location

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,8 +20,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	new_dog->name = name;
-	new_dog->age = age;
+	new_dog->name = strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->age = strdup(age);
+	if (new->age == NULL)
+	{
+		free(new->name);
+		free(new_dog);
+		return (NULL);
+	}
 	new_dog->owner = owner;
 
 	return (new_dog);
