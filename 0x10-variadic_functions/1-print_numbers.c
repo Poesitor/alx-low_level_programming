2789273e7c6fd13e7ae21cdabe0a79421d983cdd
Poesitor <Poesitor1@gmail.com>
2023-08-22 11:29:25 +0000

    specifically check for null

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 	for (i = 0; i < n - 1; i++)
-		printf("%d%s", va_arg(ap, int), (separator) ? separator : "");
+		printf("%d%s", va_arg(ap, int), (separator != NULL) ? separator : "");
 
 	printf("%d\n", va_arg(ap, int));
 	va_end(ap);
