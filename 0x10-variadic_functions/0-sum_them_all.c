b0a8e8f1e49d81fe363f42701ba5dd5964e3d2e1
Poesitor <Poesitor1@gmail.com>
2023-08-22 11:15:43 +0000

    a function that prints numbers passed to it followed by a new line

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,6 +5,7 @@
  * sum_them_all - a function that sums all its parameters
  *
  * @n: the number of parameters passed
+ * @...: The variable arguments.
  *
  * Return: the sum of all the parameters passed to the function.
  * or returns 0 if n = 0.
@@ -14,6 +15,9 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 	va_list ap;
+
+	if (n == 0)
+		return (0);
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints the numbers passed to a
+ * function followed by a new line
+ *
+ * @separator: the string to be printed between numbers
+ * @n: the number of integers passed to the function
+ *
+ * Return: Void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	if (n == 0)
+		return;
+
+	va_start(ap, n);
+	for (i = 0; i < n - 1; i++)
+		printf("%d%s", va_arg(ap, int), separator);
+
+	printf("%d\n", va_arg(ap, int));
+	va_end(ap);
+}
