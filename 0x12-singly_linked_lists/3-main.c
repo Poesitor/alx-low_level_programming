e5988b8d46bd3541dc1ea95e8b5a6cd2fbb7d42d
Poesitor <Poesitor1@gmail.com>
2023-08-24 14:19:19 +0000

    a function to add a node to the end of a list

---
+++ 0x12-singly_linked_lists/3-main.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	list_t *head;
+
+	head = NULL;
+	add_node_end(&head, "Anne");
+	add_node_end(&head, "Colton");
+	add_node_end(&head, "Victor");
+	add_node_end(&head, "Walton");
+	print_list(head);
+	return (0);
+}
