ab4a88e4ea68711f6f987cd80891b6983c2cd2f9
Poesitor <Poesitor1@gmail.com>
2023-08-24 13:34:22 +0000

    something like pushing to a linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list
+ *
+ * @head: The address of a pointer to the head
+ * @str: the string to occupy the new node
+ *
+ * Return: the address of the new elememt,
+ * or NULL if it failed.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->next = *head;
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+
+	*head = new_node;
+
+	return (new_node);
+}
