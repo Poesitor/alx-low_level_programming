63be10dfabc3959d1c5d1ed0324ec79e7e1a272b
Poesitor <Poesitor1@gmail.com>
2023-08-29 12:17:17 +0000

    fix task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,10 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *current, *tmp;
+
 	if (*head == NULL)
 		return;
-
-	listint_t *current, *tmp;
 
 	current = *head;
 	while (current != NULL)
