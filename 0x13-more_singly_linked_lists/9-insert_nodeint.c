c517a9e823243799f513ee5bb325bc917bd23475
Poesitor <Poesitor1@gmail.com>
2023-08-29 11:59:53 +0000

    check if idx is 0 in task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,9 +18,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	listint_t *current_node = *head;
 
+	if (head == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+
+	if (idx == 0)
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
 	i = 0;
 	while (i < idx && current_node != NULL)
@@ -30,7 +41,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 	}
 	if (current_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 
 	new_node->n = n;
 	new_node->next = current_node;
